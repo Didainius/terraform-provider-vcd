ed18c261085d3c71db11127d3c6d30b2c1e94300
Dainius S <dserplis@vmware.com>
2020-04-01 16:32:19 +0300

    WIP

--- go.mod
+++ go.mod
@@ -7,3 +7,4 @@ require (
 	github.com/hashicorp/terraform-plugin-sdk v1.8.0
 	github.com/vmware/go-vcloud-director/v2 v2.7.0-alpha.3
 )
+replace github.com/vmware/go-vcloud-director/v2 => ../go-vcloud-director
--- vcd/config.go
+++ vcd/config.go
@@ -3,10 +3,14 @@ package vcd
 import (
 	"crypto/sha1"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 
@@ -355,6 +359,151 @@ func ProviderAuthenticate(client *govcd.VCDClient, user, password, token, org st
 	return err
 }
 
+func ProviderAuthenticateSaml(client *govcd.VCDClient, domainUser, domainPassword, org string) error {
+
+	url := client.Client.VCDHREF
+
+	backupRedirectChecker := client.Client.Http.CheckRedirect
+	defer func() {
+		client.Client.Http.CheckRedirect = backupRedirectChecker
+	}()
+
+	client.Client.Http.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	loginUrl := url.Scheme + "://" + url.Host + "/tenant/" + org
+
+	resp, err := client.Client.Http.Get(loginUrl)
+	if err != nil {
+		return err
+	}
+
+	// https://192.168.1.160/login?service=tenant:my-org&redirectTo=%2Ftenant%2Fmy-org%2F
+	nextHop, _ := resp.Location()
+
+	resp, err = client.Client.Http.Get(nextHop.String())
+	if err != nil {
+		return err
+	}
+
+	// https://192.168.1.160/login/?service=tenant:my-org
+	nextHop, _ = resp.Location()
+
+	resp, err = client.Client.Http.Get(nextHop.String())
+	if err != nil {
+		return err
+	}
+
+	// https://192.168.1.160/login/my-org/saml/login/alias/vcd?service=tenant:my-org
+	nextHop, _ = resp.Location()
+	// https://win-60g606n0afg.test-forest.net/adfs/ls/?SAMLRequest=fZFNT8MwDIbv%2FIoq9zYflDKitdMEQkICCcHgwM1K3S5Tm4w4HfDvyQYTcOESK9Lrx86T%2BeJ9HLIdBrLe1UwWgmXojG%2Bt62v2tLrOZ2zRnMwJxkFt9XKKa%2FeArxNSzJZEGGLqu%2FSOphHDI4adNXjjWnyvWSJdpZh1EA%2FsdYxb0py%2FWZdXoq9E5QR0fRFTKO98SKVwGDm0HfGBOMuufTB4GFmzDgZClt1c1QxKs5mhrJSFslRdrzYbgWDkWQVnQp6mEN0Dkd3hTxvRlPaiCC7WTAklclHmQq7EuVZKy4uinJ2%2BsOz5KELtRSQ1jvTX02s2Bac9kCXtYETS0ejH5d2tTlG9DT564wfWfJnSh4HhN%2BF%2FABxdsuboSV6oQlazQqZTcDP4qeU%2B9Hz8yPdlD%2BUjRmghAofBAvGdaef89wLN9%2FXvzzWf&RelayState=aHR0cHM6Ly8xOTIuMTY4LjEuMTYwL3RlbmFudC9teS1vcmc%3D&SigAlg=http%3A%2F%2Fwww.w3.org%2F2000%2F09%2Fxmldsig%23rsa-sha1&Signature=W3mUfGiecEJudJLqIV%2F2cFuJcbPiQxgayxVbJf6hOMp8ZQcqG01NR1Rm3qTqaote5dSkprw42dVOMIHdeiJL1g7%2FW9ON6%2BvzJHvL3rdy652%2BeSv6q0r9wDJ8eKC5DpwcmW0UUATHHt4ENMPa6w6MgE2Mwm1F6eYu1c5CcIC306lzQNiSWwNA08frX1wxl3RtSrrm9qo9K9UoQOAULkYjAgghI65Dr%2BEWjiu%2FYgVw1SuXMKRQcQ1Q8MQ2uhDqjlfXuO3Fnp582zLh1uMx1ZiFO1LPaqTT7K%2BBvlUISzkCe6YkSOlr%2Fz08t7A4fkMxmhnTd2gBu6WhzcUUWkqLMzSwgw%3D%3D
+
+	resp, err = client.Client.Http.Get(nextHop.String())
+	if err != nil {
+		return err
+	}
+
+	nextHop, _ = resp.Location()
+
+	log.Println("DAINIUS nexthop ", nextHop)
+
+	authEndPoint := nextHop.Scheme + "://" + nextHop.Hostname() + "/adfs/services/trust/13/usernamemixed"
+
+	body := strings.NewReader(requestBody)
+
+	log.Println("DAINIUS posting to endpoint: ", authEndPoint)
+
+	resp, err = client.Client.Http.Post(authEndPoint, "application/soap+xml", body)
+	if err != nil {
+		return err
+	}
+
+	log.Println("DAINIUS auth body: ", requestBody)
+
+	log.Println("DAINIUS response status: ", resp.StatusCode)
+	log.Printf("DAINIUS response %+#v", resp)
+
+	r, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Println("DAINIUS auth response: ", string(r))
+	return nil
+}
+
+var requestBody = `<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+  <s:Header>
+    <a:Action s:mustUnderstand="1">http://docs.oasis-open.org/ws-sx/ws-trust/200512/RST/Issue</a:Action>
+    <a:ReplyTo>
+      <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+    </a:ReplyTo>
+    <a:To s:mustUnderstand="1">https://win-60g606n0afg.test-forest.net/adfs/services/trust/13/usernamemixed</a:To>
+    <o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+      <u:Timestamp u:Id="_0">
+        <u:Created>` + time.Now().Format(time.RFC3339) + `</u:Created>
+        <u:Expires>` + time.Now().Add(1*time.Minute).Format(time.RFC3339) + `</u:Expires>
+      </u:Timestamp>
+      <o:UsernameToken>
+        <o:Username>test@test-forest.net</o:Username>
+        <o:Password o:Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">R00tas!23</o:Password>
+      </o:UsernameToken>
+    </o:Security>
+  </s:Header>
+  <s:Body>
+    <trust:RequestSecurityToken xmlns:trust="http://docs.oasis-open.org/ws-sx/ws-trust/200512">
+      <wsp:AppliesTo xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy">
+        <a:EndpointReference>
+          <a:Address>https://192.168.1.160/cloud/org/my-org/saml/metadata/alias/vcd</a:Address>
+        </a:EndpointReference>
+      </wsp:AppliesTo>
+      <trust:KeySize>0</trust:KeySize>
+      <trust:KeyType>http://docs.oasis-open.org/ws-sx/ws-trust/200512/Bearer</trust:KeyType>
+      <i:RequestDisplayToken xml:lang="en" xmlns:i="http://schemas.xmlsoap.org/ws/2005/05/identity" />
+      <trust:RequestType>http://docs.oasis-open.org/ws-sx/ws-trust/200512/Issue</trust:RequestType>
+      <trust:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</trust:TokenType>
+    </trust:RequestSecurityToken>
+  </s:Body>
+</s:Envelope>
+`
+
+const requestBodyTemplate = `
+<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+  <s:Header>
+    <a:Action s:mustUnderstand="1">http://docs.oasis-open.org/ws-sx/ws-trust/200512/RST/Issue</a:Action>
+    <a:ReplyTo>
+      <a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address>
+    </a:ReplyTo>
+    <a:To s:mustUnderstand="1">{{.DestinationUrl}}</a:To>
+    <o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+      <u:Timestamp u:Id="_0">
+        <u:Created>{{.TsUtcNow}}</u:Created>
+        <u:Expires>{{.TsUtcThen}}</u:Expires>
+      </u:Timestamp>
+      <o:UsernameToken>
+        <o:Username>{{.Username}}</o:Username>
+        <o:Password o:Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">{{.Password}}</o:Password>
+      </o:UsernameToken>
+    </o:Security>
+  </s:Header>
+  <s:Body>
+    <trust:RequestSecurityToken xmlns:trust="http://docs.oasis-open.org/ws-sx/ws-trust/200512">
+      <wsp:AppliesTo xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy">
+        <a:EndpointReference>
+          <a:Address>{{.EntityId}}</a:Address>
+        </a:EndpointReference>
+      </wsp:AppliesTo>
+      <trust:KeySize>0</trust:KeySize>
+      <trust:KeyType>http://docs.oasis-open.org/ws-sx/ws-trust/200512/Bearer</trust:KeyType>
+      <i:RequestDisplayToken xml:lang="en" xmlns:i="http://schemas.xmlsoap.org/ws/2005/05/identity" />
+      <trust:RequestType>http://docs.oasis-open.org/ws-sx/ws-trust/200512/Issue</trust:RequestType>
+      <trust:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</trust:TokenType>
+    </trust:RequestSecurityToken>
+  </s:Body>
+</s:Envelope>
+`
+
 func (c *Config) Client() (*VCDClient, error) {
 	rawData := c.User + "#" +
 		c.Password + "#" +
@@ -397,7 +546,8 @@ func (c *Config) Client() (*VCDClient, error) {
 		MaxRetryTimeout: c.MaxRetryTimeout,
 		InsecureFlag:    c.InsecureFlag}
 
-	err = ProviderAuthenticate(vcdClient.VCDClient, c.User, c.Password, c.Token, c.SysOrg)
+	// err = ProviderAuthenticate(vcdClient.VCDClient, c.User, c.Password, c.Token, c.SysOrg)
+	err = ProviderAuthenticateSaml(vcdClient.VCDClient, c.User, c.Password, c.SysOrg)
 	if err != nil {
 		return nil, fmt.Errorf("something went wrong during authentication: %s", err)
 	}
