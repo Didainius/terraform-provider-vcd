515b9f6425411a41154799d231ff16f2681093b3
Dainius Serplis <dserplis@vmware.com>
2021-06-15 15:46:27 +0300

    Single resource
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- vcd/resource_vcd_nsxt_ipsec_vpn.go
+++ vcd/resource_vcd_nsxt_ipsec_vpn.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
 	"github.com/vmware/go-vcloud-director/v2/govcd"
 
 	"github.com/vmware/go-vcloud-director/v2/types/v56"
@@ -96,17 +98,16 @@ func resourceVcdNsxtIpSecVpnTunnel() *schema.Resource {
 				Default:     false,
 				Description: "Sets whether logging for the tunnel is enabled or not. (default - false)",
 			},
-
 			"security_profile": &schema.Schema{
 				Type:     schema.TypeString,
 				Computed: true,
-				Description: "Security type which is use for IPSec VPN Tunnel. It will be 'DEFAULT' if nothing is " +
+				Description: "Security type which is use for IPsec VPN Tunnel. It will be 'DEFAULT' if nothing is " +
 					"customized and 'CUSTOM' if some changes are applied",
 			},
 			"status": &schema.Schema{
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "Overall IPSec VPN Tunnel Status",
+				Description: "Overall IPsec VPN Tunnel Status",
 			},
 			"ike_service_status": &schema.Schema{
 				Type:        schema.TypeString,
@@ -117,6 +118,103 @@ func resourceVcdNsxtIpSecVpnTunnel() *schema.Resource {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "Provides more details of failure if the IKE service is not UP",
+			},
+
+			"security_profile_customization": &schema.Schema{
+				Type:        schema.TypeList,
+				Optional:    true,
+				MaxItems:    1,
+				Description: "Security profile customization",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+
+						"ike_version": &schema.Schema{
+							Type:         schema.TypeString,
+							Required:     true,
+							Description:  "",
+							ValidateFunc: validation.StringInSlice([]string{"IKE_V1", "IKE_V2", "IKE_FLEX"}, false),
+						},
+						"ike_encryption_algorithms": &schema.Schema{
+							Type:        schema.TypeSet,
+							Required:    true,
+							Description: "",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"ike_digest_algorithms": &schema.Schema{
+							Type:        schema.TypeSet,
+							Optional:    true,
+							Description: "",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"ike_dh_groups": &schema.Schema{
+							Type:        schema.TypeSet,
+							Required:    true,
+							Description: "",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"ike_sa_lifetime": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Security Association life time (in seconds)",
+						},
+
+						"tunnel_pfs_enabled": &schema.Schema{
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Default:     true,
+							Description: "Perfect Forward Secrecy",
+						},
+
+						"tunnel_df_policy": &schema.Schema{
+							Type:         schema.TypeString,
+							Optional:     true,
+							Default:      "COPY",
+							Description:  "Perfect Forward Secrecy",
+							ValidateFunc: validation.StringInSlice([]string{"COPY", "CLEAR"}, false),
+						},
+
+						"tunnel_encryption_algorithms": &schema.Schema{
+							Type:        schema.TypeSet,
+							Required:    true,
+							Description: "",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"tunnel_digest_algorithms": &schema.Schema{
+							Type:        schema.TypeSet,
+							Optional:    true,
+							Description: "",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"tunnel_dh_groups": &schema.Schema{
+							Type:        schema.TypeSet,
+							Required:    true,
+							Description: "",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"tunnel_sa_lifetime": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Security Association life time (in seconds)",
+						},
+						"dpd_probe_internal": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "Dead Peer Detection probe interval (in seconds)",
+						},
+					},
+				},
 			},
 		},
 	}
@@ -138,15 +236,29 @@ func resourceVcdNsxtIpSecVpnTunnelCreate(ctx context.Context, d *schema.Resource
 
 	ipSecVpnConfig, err := getNsxtIpSecVpnTunnelType(d)
 	if err != nil {
-		return diag.Errorf("error getting NSX-T IPSec VPN Tunnel configuration type: %s", err)
+		return diag.Errorf("error getting NSX-T IPsec VPN Tunnel configuration type: %s", err)
 	}
 
 	createdIpSecVpnConfig, err := nsxtEdge.CreateIpSecVpn(ipSecVpnConfig)
 	if err != nil {
-		return diag.Errorf("error creating NSX-T IPSec VPN Tunnel configuration: %s", err)
+		return diag.Errorf("error creating NSX-T IPsec VPN Tunnel configuration: %s", err)
 	}
-
+	// IPSec VPN Tunnel is already create - store the ID
 	d.SetId(createdIpSecVpnConfig.NsxtIpSecVpn.ID)
+
+	// Tunnel Profile
+	if _, isSet := d.GetOk("security_profile_customization"); isSet {
+		tunnelProfileConfig, err := getNsxtIpSecVpnProfileTunnelConfigurationType(d)
+		if err != nil {
+			return diag.Errorf("error getting NSX-T IPsec VPN Tunnel Profile: %s", err)
+		}
+
+		_, err = createdIpSecVpnConfig.UpdateTunnelConnectionProperties(tunnelProfileConfig)
+		if err != nil {
+			return diag.Errorf("error setting VPN Tunnel Profile: %s", err)
+		}
+
+	}
 
 	return resourceVcdNsxtIpSecVpnTunnelRead(ctx, d, meta)
 }
@@ -165,16 +277,55 @@ func resourceVcdNsxtIpSecVpnTunnelUpdate(ctx context.Context, d *schema.Resource
 		return diag.Errorf("error retrieving Edge Gateway: %s", err)
 	}
 
+	// Retrieve configuration
+	existingIpSecVpnConfiguration, err := nsxtEdge.GetIpSecVpnById(d.Id())
+	if err != nil {
+		diag.Errorf("error retrieving existing NSX-T IPsec VPN Tunnel configuration: %s", err)
+	}
+
 	ipSecVpnConfig, err := getNsxtIpSecVpnTunnelType(d)
 	if err != nil {
-		return diag.Errorf("error getting NSX-T IPSec VPN Tunnel configuration type: %s", err)
+		return diag.Errorf("error getting NSX-T IPsec VPN Tunnel configuration type: %s", err)
 	}
 	// Inject ID for update
 	ipSecVpnConfig.ID = d.Id()
 
-	_, err = nsxtEdge.CreateIpSecVpn(ipSecVpnConfig)
+	//
+	securityProfileHasChange := d.HasChange("security_profile_customization")
+	_, newSecurityProfile := d.GetChange("security_profile_customization")
+
+	// Security Profile Customization settings work on two different endpoints:
+	// * To set a custom security profile - there is a separate endpoint where all security profile settings can be
+	// set. After setting them, parent IPsec VPN Tunnel `SecurityType` becomes "CUSTOM".
+	// * To remove customization and switch back to NSX-T Default parameters the parent IPsec VPN Tunnel must be updated
+	// and its field 'SecurityType' must be set to 'DEFAULT'
+	if securityProfileHasChange && len(newSecurityProfile.([]interface{})) == 0 {
+		ipSecVpnConfig.SecurityType = "DEFAULT"
+	}
+
+	// At first update IPsec VPN tunnel configuration
+	// It will reset Security Profile to DEFAULT at the same shot if no customization exists in 'security_profile_customization'
+	updatedIpSecVpnConfiguration, err := existingIpSecVpnConfiguration.Update(ipSecVpnConfig)
 	if err != nil {
-		return diag.Errorf("error updating NSX-T IPSec VPN Tunnel configuration '%s': %s", ipSecVpnConfig.Name, err)
+		return diag.Errorf("error updating NSX-T IPsec VPN Tunnel configuration '%s': %s", ipSecVpnConfig.Name, err)
+	}
+
+	// If Security Profile has change - it must be explicitly set using UpdateTunnelConnectionProperties which will
+	// change value of parent NsxtIpSecVpnTunnel.SecurityType to 'CUSTOM' automatically.
+	if securityProfileHasChange && newSecurityProfile != nil {
+		ipSecTunnelProfileConfig, err := getNsxtIpSecVpnProfileTunnelConfigurationType(d)
+		if err != nil {
+			return diag.Errorf("error getting NSX-T IPsec VPN Tunnel Profile: %s", err)
+		}
+
+		// To set IPsec VPN Tunnel Connection Profile - it must be updated (HTTP PUT) with all the options configured
+		if ipSecTunnelProfileConfig != nil {
+			_, err = updatedIpSecVpnConfiguration.UpdateTunnelConnectionProperties(ipSecTunnelProfileConfig)
+			if err != nil {
+				return diag.Errorf("error setting NSX-T IPsec VPN Tunnel Profile: %s", err)
+			}
+
+		}
 	}
 
 	return resourceVcdNsxtIpSecVpnTunnelRead(ctx, d, meta)
@@ -197,19 +348,30 @@ func resourceVcdNsxtIpSecVpnTunnelRead(ctx context.Context, d *schema.ResourceDa
 		if govcd.ContainsNotFound(err) {
 			d.SetId("")
 		}
-		return diag.Errorf("error retrieving NSX-T IPSec VPN Tunnel configuration for deletion: %s", err)
+		return diag.Errorf("error retrieving NSX-T IPsec VPN Tunnel configuration for deletion: %s", err)
 	}
 
 	// Set general schema for configuration
 	err = setNsxtIpSecVpnTunnelData(d, ipSecVpnConfig.NsxtIpSecVpn)
 	if err != nil {
-		return diag.Errorf("error storing NSX-T IPSec VPN Tunnel configuration to schema: %s", err)
+		return diag.Errorf("error storing NSX-T IPsec VPN Tunnel configuration to schema: %s", err)
+	}
+
+	// ipSe
+	tunnelConnectionProperties, err := ipSecVpnConfig.GetTunnelConnectionProperties()
+	if err != nil {
+		return diag.Errorf("error reading NSX-T IPsec VPN Tunnel Security Customization: %s", err)
+	}
+
+	err = setNsxtIpSecVpnProfileTunnelConfigurationData(d, tunnelConnectionProperties)
+	if err != nil {
+		return diag.Errorf("error storing NSX-T IPsec VPN Tunnel Security Customization to schema: %s", err)
 	}
 
 	// Read tunnel status data from separate endpoint
 	tunnelStatus, err := ipSecVpnConfig.GetStatus()
 	if err != nil {
-		return diag.Errorf("error reading NSX-T IPSec VPN Tunnel status: %s", err)
+		return diag.Errorf("error reading NSX-T IPsec VPN Tunnel status: %s", err)
 	}
 	setNsxtIpSecVpnTunnelStatusData(d, tunnelStatus)
 
@@ -232,12 +394,12 @@ func resourceVcdNsxtIpSecVpnTunnelDelete(ctx context.Context, d *schema.Resource
 
 	ipSecVpnConfig, err := nsxtEdge.GetIpSecVpnById(d.Id())
 	if err != nil {
-		return diag.Errorf("error retrieving NSX-T IPSec VPN Tunnel configuration for deletion: %s", err)
+		return diag.Errorf("error retrieving NSX-T IPsec VPN Tunnel configuration for deletion: %s", err)
 	}
 
 	err = ipSecVpnConfig.Delete()
 	if err != nil {
-		return diag.Errorf("error deleting NSX-T IPSec VPN Tunnel configuration: %s", err)
+		return diag.Errorf("error deleting NSX-T IPsec VPN Tunnel configuration: %s", err)
 	}
 
 	d.SetId("")
@@ -308,4 +470,82 @@ func setNsxtIpSecVpnTunnelStatusData(d *schema.ResourceData, ipSecVpnStatus *typ
 	_ = d.Set("ike_service_status", ipSecVpnStatus.IkeStatus.IkeServiceStatus)
 	_ = d.Set("ike_fail_reason", ipSecVpnStatus.IkeStatus.FailReason)
 	return
+}
+
+func getNsxtIpSecVpnProfileTunnelConfigurationType(d *schema.ResourceData) (*types.NsxtIpSecVpnTunnelSecurityProfile, error) {
+	tunnel, isSet := d.GetOk("security_profile_customization")
+
+	if !isSet {
+		return nil, nil
+	}
+	tunnelSlice := tunnel.([]interface{})
+	tunnelMap := tunnelSlice[0].(map[string]interface{})
+
+	nsxtIpSecVpnTunnelProfile := &types.NsxtIpSecVpnTunnelSecurityProfile{
+		SecurityType: "CUSTOM", // Security Type must become CUSTOM, because we are configuring profile
+		IkeConfiguration: types.NsxtIpSecVpnTunnelProfileIkeConfiguration{
+			IkeVersion:           tunnelMap["ike_version"].(string),
+			EncryptionAlgorithms: convertSchemaSetToSliceOfStrings(tunnelMap["ike_encryption_algorithms"].(*schema.Set)),
+			DigestAlgorithms:     convertSchemaSetToSliceOfStrings(tunnelMap["ike_digest_algorithms"].(*schema.Set)),
+			DhGroups:             convertSchemaSetToSliceOfStrings(tunnelMap["ike_dh_groups"].(*schema.Set)),
+			SaLifeTime:           tunnelMap["ike_sa_lifetime"].(int),
+		},
+		TunnelConfiguration: types.NsxtIpSecVpnTunnelProfileTunnelConfiguration{
+			PerfectForwardSecrecyEnabled: tunnelMap["tunnel_pfs_enabled"].(bool),
+			DfPolicy:                     tunnelMap["tunnel_df_policy"].(string),
+			EncryptionAlgorithms:         convertSchemaSetToSliceOfStrings(tunnelMap["tunnel_encryption_algorithms"].(*schema.Set)),
+			DigestAlgorithms:             convertSchemaSetToSliceOfStrings(tunnelMap["tunnel_digest_algorithms"].(*schema.Set)),
+			DhGroups:                     convertSchemaSetToSliceOfStrings(tunnelMap["tunnel_dh_groups"].(*schema.Set)),
+			SaLifeTime:                   tunnelMap["tunnel_sa_lifetime"].(int),
+		},
+		DpdConfiguration: types.NsxtIpSecVpnTunnelProfileDpdConfiguration{
+			ProbeInterval: tunnelMap["dpd_probe_internal"].(int),
+		},
+	}
+
+	return nsxtIpSecVpnTunnelProfile, nil
+}
+
+func converListTotTypeSet(slice []string) *schema.Set {
+	sliceOfInterfaces := convertToTypeSet(slice)
+	set := schema.NewSet(schema.HashSchema(&schema.Schema{Type: schema.TypeString}), sliceOfInterfaces)
+
+	return set
+}
+
+func setNsxtIpSecVpnProfileTunnelConfigurationData(d *schema.ResourceData, tunneConfig *types.NsxtIpSecVpnTunnelSecurityProfile) error {
+
+	if tunneConfig.SecurityType == "DEFAULT" {
+		err := d.Set("security_profile_customization", nil)
+		if err != nil {
+			return fmt.Errorf("error resetting 'security_profile_customization' to empty: %s", err)
+		}
+		// Return early because there is nothing to store
+		return nil
+	}
+
+	object := make([]interface{}, 1)
+	objectMap := make(map[string]interface{})
+
+	objectMap["ike_version"] = tunneConfig.IkeConfiguration.IkeVersion
+
+	objectMap["ike_encryption_algorithms"] = converListTotTypeSet(tunneConfig.IkeConfiguration.EncryptionAlgorithms)
+	objectMap["ike_digest_algorithms"] = converListTotTypeSet(tunneConfig.IkeConfiguration.DigestAlgorithms)
+	objectMap["ike_dh_groups"] = converListTotTypeSet(tunneConfig.IkeConfiguration.DhGroups)
+	objectMap["ike_sa_lifetime"] = tunneConfig.IkeConfiguration.SaLifeTime
+
+	objectMap["tunnel_pfs_enabled"] = tunneConfig.TunnelConfiguration.PerfectForwardSecrecyEnabled
+	objectMap["tunnel_df_policy"] = tunneConfig.TunnelConfiguration.DfPolicy
+	objectMap["tunnel_encryption_algorithms"] = converListTotTypeSet(tunneConfig.TunnelConfiguration.EncryptionAlgorithms)
+	objectMap["tunnel_digest_algorithms"] = converListTotTypeSet(tunneConfig.TunnelConfiguration.DigestAlgorithms)
+	objectMap["tunnel_dh_groups"] = converListTotTypeSet(tunneConfig.TunnelConfiguration.DhGroups)
+	objectMap["tunnel_sa_lifetime"] = tunneConfig.TunnelConfiguration.SaLifeTime
+
+	objectMap["dpd_probe_internal"] = tunneConfig.DpdConfiguration.ProbeInterval
+
+	object[0] = objectMap
+
+	err := d.Set("security_profile_customization", object)
+
+	return err
 }
--- vcd/resource_vcd_nsxt_ipsec_vpn_test.go
+++ vcd/resource_vcd_nsxt_ipsec_vpn_test.go
@@ -160,25 +160,194 @@ resource "vcd_nsxt_ipsec_vpn_tunnel" "tunnel1" {
   # That is a fake remote IP address
   remote_ip_address = "1.2.3.4"
   remote_networks   = ["192.168.1.0/24", "192.168.10.0/24", "192.168.20.0/28"]
+}
+`
 
-  security_profile {
-    # IKE Profiles
-    version              = ""
-    encryption           = ""
-    digest               = ""
-    diffie_hellman_group = ""
-    associated_lifetime  = ""
+func TestAccVcdNsxtIpSecVpnTunnelCustomProfile(t *testing.T) {
+	preTestChecks(t)
+	skipNoNsxtConfiguration(t)
+
+	// String map to fill the template
+	var params = StringMap{
+		"Org":         testConfig.VCD.Org,
+		"NsxtVdc":     testConfig.Nsxt.Vdc,
+		"EdgeGw":      testConfig.Nsxt.EdgeGateway,
+		"NetworkName": t.Name(),
+		"Tags":        "network nsxt",
+	}
+
+	configText1 := templateFill(testAccNsxtIpSecVpnTunnelProfileStep1, params)
+	debugPrintf("#[DEBUG] CONFIGURATION for step 1: %s", configText1)
+
+	params["FuncName"] = t.Name() + "-step2"
+	configText2 := templateFill(testAccNsxtIpSecVpnTunnelProfileStep2, params)
+	debugPrintf("#[DEBUG] CONFIGURATION for step 2: %s", configText2)
+	//
+	//params["FuncName"] = t.Name() + "-step11"
+	//configText11 := templateFill(testAccNsxtIpSetEmpty2+testAccNsxtIpSetDS, params)
+	//debugPrintf("#[DEBUG] CONFIGURATION for step 3: %s", configText11)
+	//
+	//params["FuncName"] = t.Name() + "-step2"
+	//configText2 := templateFill(testAccNsxtIpSetIpRanges, params)
+	//debugPrintf("#[DEBUG] CONFIGURATION for step 4: %s", configText2)
+
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testAccProviders,
+		PreCheck:          func() { testAccPreCheck(t) },
+		//CheckDestroy: resource.ComposeAggregateTestCheckFunc(
+		//	testAccCheckNsxtFirewallGroupDestroy(testConfig.Nsxt.Vdc, "test-ip-set", types.FirewallGroupTypeIpSet),
+		//	testAccCheckNsxtFirewallGroupDestroy(testConfig.Nsxt.Vdc, "test-ip-set-changed", types.FirewallGroupTypeIpSet),
+		//),
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: configText1,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("vcd_nsxt_ipsec_vpn_tunnel.tunnel1", "id"),
+					resource.TestCheckResourceAttr("vcd_nsxt_ipsec_vpn_tunnel.tunnel1", "security_profile_customization.#", "1"),
+					//stateDumper(),
+					//sleepTester(),
+					//resource.TestMatchResourceAttr("vcd_nsxt_ip_set.set1", "id", regexp.MustCompile(`^urn:vcloud:firewallGroup:.*$`)),
+					//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "name", "test-ip-set"),
+					//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "description", "test-ip-set-description"),
+					//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "ip_addresses.#", "0"),
+				),
+			},
+			resource.TestStep{
+				Config: configText2,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("vcd_nsxt_ipsec_vpn_tunnel.tunnel1", "id"),
+					resource.TestCheckResourceAttr("vcd_nsxt_ipsec_vpn_tunnel.tunnel1", "security_profile_customization.#", "0"),
+
+					//stateDumper(),
+					//sleepTester(),
+					//resource.TestMatchResourceAttr("vcd_nsxt_ip_set.set1", "id", regexp.MustCompile(`^urn:vcloud:firewallGroup:.*$`)),
+					//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "name", "test-ip-set"),
+					//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "description", "test-ip-set-description"),
+					//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "ip_addresses.#", "0"),
+				),
+			},
+			//resource.TestStep{
+			//	Config: configText1,
+			//	Check: resource.ComposeAggregateTestCheckFunc(
+			//		resource.TestCheckResourceAttrSet("vcd_nsxt_ipsec_vpn_tunnel.tunnel1", "id"),
+			//		resource.TestCheckResourceAttrSet("vcd_nsxt_ipsec_vpn_tunnel.tunnel1", "status"),
+			//		//resource.TestMatchResourceAttr("vcd_nsxt_ip_set.set1", "id", regexp.MustCompile(`^urn:vcloud:firewallGroup:.*$`)),
+			//		//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "name", "test-ip-set"),
+			//		//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "description", "test-ip-set-description"),
+			//		//resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "ip_addresses.#", "0"),
+			//	),
+			//},
+			//resource.TestStep{
+			//	Config: configText2,
+			//	Check: resource.ComposeAggregateTestCheckFunc(
+			//		resource.TestMatchResourceAttr("vcd_nsxt_ip_set.set1", "id", regexp.MustCompile(`^urn:vcloud:firewallGroup:.*$`)),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "name", "test-ip-set-changed"),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "description", ""),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "ip_addresses.#", "0"),
+			//	),
+			//},
+			//resource.TestStep{
+			//	Config: configText11,
+			//	Check: resource.ComposeAggregateTestCheckFunc(
+			//		resource.TestMatchResourceAttr("vcd_nsxt_ip_set.set1", "id", regexp.MustCompile(`^urn:vcloud:firewallGroup:.*$`)),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "name", "test-ip-set-changed"),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "description", ""),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "ip_addresses.#", "0"),
+			//
+			//		resourceFieldsEqual("vcd_nsxt_ip_set.set1", "data.vcd_nsxt_ip_set.ds", []string{}),
+			//	),
+			//},
+			//// Test import with no IP addresses
+			//resource.TestStep{
+			//	ResourceName:      "vcd_nsxt_ip_set.set1",
+			//	ImportState:       true,
+			//	ImportStateVerify: true,
+			//	ImportStateIdFunc: importStateIdNsxtEdgeGatewayObject(testConfig, testConfig.Nsxt.EdgeGateway, "test-ip-set-changed"),
+			//},
+			//resource.TestStep{
+			//	Config: configText2,
+			//	Check: resource.ComposeAggregateTestCheckFunc(
+			//		resource.TestMatchResourceAttr("vcd_nsxt_ip_set.set1", "id", regexp.MustCompile(`^urn:vcloud:firewallGroup:.*$`)),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "name", "test-ip-set-changed"),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "description", ""),
+			//		resource.TestCheckTypeSetElemAttr("vcd_nsxt_ip_set.set1", "ip_addresses.*", "12.12.12.1"),
+			//		resource.TestCheckTypeSetElemAttr("vcd_nsxt_ip_set.set1", "ip_addresses.*", "10.10.10.0/24"),
+			//		resource.TestCheckTypeSetElemAttr("vcd_nsxt_ip_set.set1", "ip_addresses.*", "11.11.11.1-11.11.11.2"),
+			//		resource.TestCheckTypeSetElemAttr("vcd_nsxt_ip_set.set1", "ip_addresses.*", "2001:db8::/48"),
+			//		resource.TestCheckTypeSetElemAttr("vcd_nsxt_ip_set.set1", "ip_addresses.*", "2001:db6:0:0:0:0:0:0-2001:db6:0:ffff:ffff:ffff:ffff:ffff"),
+			//		resource.TestCheckResourceAttr("vcd_nsxt_ip_set.set1", "ip_addresses.#", "5"),
+			//	),
+			//},
+			//// Test import with IP addresses
+			//resource.TestStep{
+			//	ResourceName:      "vcd_nsxt_ip_set.set1",
+			//	ImportState:       true,
+			//	ImportStateVerify: true,
+			//	ImportStateIdFunc: importStateIdNsxtEdgeGatewayObject(testConfig, testConfig.Nsxt.EdgeGateway, "test-ip-set-changed"),
+			//},
+		},
+	})
+	postTestChecks(t)
+}
+
+const testAccNsxtIpSecVpnTunnelProfileStep1 = testAccNsxtIpSetPrereqs + `
+resource "vcd_nsxt_ipsec_vpn_tunnel" "tunnel1" {
+  org = "{{.Org}}"
+  vdc = "{{.NsxtVdc}}"
+
+  edge_gateway_id = data.vcd_nsxt_edgegateway.existing_gw.id
+
+  name        = "test-tunnel-1"
+  description = "test-tunnel-description"
+  
+  pre_shared_key    = "test-psk"
+  # Primary IP address of Edge Gateway
+  local_ip_address  = tolist(data.vcd_nsxt_edgegateway.existing_gw.subnet)[0].primary_ip
+  local_networks    = ["10.10.10.0/24", "30.30.30.0/28", "40.40.40.1/32"]
+  # That is a fake remote IP address as there is nothing else to peer to
+  remote_ip_address = "1.2.3.4"
+  remote_networks   = ["192.168.1.0/24", "192.168.10.0/24", "192.168.20.0/28"]
+
+  security_profile_customization {
+    ike_version               = "IKE_V2"
+    ike_encryption_algorithms = ["AES_128"]
+    ike_digest_algorithms     = ["SHA2_256"]
+    ike_dh_groups             = ["GROUP14"]
+    ike_sa_lifetime           = 86400
     
-    # Tunnel configuration 
-    enable_perfect_forward_secrecy = ""
-    defragmentation_policy         = ""
-    encryption                     = ""
-    digest                         = ""
-    diffie_hellman_group = ""
-    associated_lifetime  = ""
+	tunnel_pfs_enabled = true
+	tunnel_df_policy = "COPY"
+    tunnel_encryption_algorithms = ["AES_GCM_128"]
+    # tunnel_digest_algorithms     = ["AES_GCM_128"]
+    tunnel_dh_groups             = ["GROUP14"]
+    tunnel_sa_lifetime           = 3600
     
-    # DPD Configuration
-    probe_internal = ""
+    dpd_probe_internal = "30"
   }
+}
+`
+
+const testAccNsxtIpSecVpnTunnelProfileStep2 = testAccNsxtIpSetPrereqs + `
+resource "vcd_nsxt_ipsec_vpn_tunnel" "tunnel1" {
+  org = "{{.Org}}"
+  vdc = "{{.NsxtVdc}}"
+
+  edge_gateway_id = data.vcd_nsxt_edgegateway.existing_gw.id
+
+  name        = "test-tunnel-1"
+  description = "test-tunnel-description"
+  
+  pre_shared_key    = "test-psk"
+  # Primary IP address of Edge Gateway
+  local_ip_address  = tolist(data.vcd_nsxt_edgegateway.existing_gw.subnet)[0].primary_ip
+  local_networks    = ["10.10.10.0/24", "30.30.30.0/28", "40.40.40.1/32"]
+  # That is a fake remote IP address as there is nothing else to peer to
+  remote_ip_address = "1.2.3.4"
+  remote_networks   = ["192.168.1.0/24", "192.168.10.0/24", "192.168.20.0/28"]
 }
 `
